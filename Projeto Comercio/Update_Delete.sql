9ab26c7cad7be6a6e5307dc7c8aafb552d54fce3
--global <--global>
2024-06-29 16:22:58 -0300

    projeto-comercio(Update-delete): alterando tabelas.

---
+++ Projeto Comercio/Update_Delete.sql
@@ -0,0 +1,13 @@
+UPDATE cliente
+SET nome = 'Linda'
+WHERE nome = 'Lindinha';
+
+UPDATE cliente
+SET nomeSocial = 'Estressada'
+WHERE id_cliente = 8;
+
+DELETE FROM cliente
+WHERE nome = 'tio Patinhas';
+
+DELETE FROM cliente
+WHERE id_cliente = 7;
